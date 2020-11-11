896e02cd528ea41321de1160ae3c57937922d5ee
G Strick <graysonstricker@gmail.com>
2020-11-11 17:37:26 -0500

    made changes to the deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    plugins: [
-      {
-        resolve: "@wkocjan/gatsby-theme-intro",
-        options: {
-          theme: "classic",
-        },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "classic",
       },
-    ],
-    pathPrefix: `/My-Portfolio`,
-  }
+    },
+  ],
+  pathPrefix: `/Portfolio-Website`,
+};
